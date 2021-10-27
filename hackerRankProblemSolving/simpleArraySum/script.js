7c3b6c1df2ce4d1b345aa0efbae5aac00cafd6bf
Ali Hassan <bc200401060@vu.edu.pk>
2021-10-28 03:55:26 +0500

    Updating Simple Array Sum code

--- hackerRankProblemSolving/simpleArraySum/script.js
+++ hackerRankProblemSolving/simpleArraySum/script.js
@@ -2,7 +2,8 @@ function simpleArraySum(ar) {
   let sum = 0;
 
    for(let i = 0; i < ar.length; i++) {
-       sum = sum + ar[i];
+      //  sum = sum + ar[i]; or
+       sum = ar.reduce((a, b) => a + b);
    }
 
    return sum;
