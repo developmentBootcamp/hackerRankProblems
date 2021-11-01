3e5e498887f252a5722c58436a233185de188a45
Ali Hassan <bc200401060@vu.edu.pk>
2021-11-01 06:53:36 +0500

    updating day 3 files

--- hackerRankJs/tenDayJs/day3/problem1.js
+++ hackerRankJs/tenDayJs/day3/problem1.js
@@ -1,7 +1,7 @@
 function getSecondLargest(nums) {
     let maxNum = Math.max.apply(null, nums);
     let result;
-    for (var i = nums.length - 1; i >= 0; i--) {
+    for (let i = nums.length - 1; i >= 0; i--) {
         if (nums[i] === maxNum) {
             nums.splice(i, 1);
         }
